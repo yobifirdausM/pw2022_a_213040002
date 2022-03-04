d1bd3cd0d41e8c0d5f4e9f5fe46977b3e1e9c1e1
yobifirdausM <yobifirdaus03@gmail.com>
2022-03-04 20:51:35 +0700

    mengerjakan pertemuan 3

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,38 @@
+<?php
+// pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusu array
+
+// for( $i = 0; $i < 5; $i++ ) {
+//     echo "Hello Word! <br>";
+// }
+// $i = 10;
+// while( $i < 5 ) {
+//     echo "Hello World ! <br>";
+// $i++;
+// }
+// $i = 10;
+// do {
+//     echo "Hello World ! <br>";
+// $i++;
+// } while( $i < 5 );
+
+// Pengkondisian / percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 2;
+if( $x < 20 ) {
+    echo "Benar";
+} else if( $x == 20 ) {
+    echo "bingo!";
+} else {
+    echo "Salah";
+}
+
+
+?>
